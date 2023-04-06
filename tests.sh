68a48f4a92c4b4a7dbc2853e94f4f720d307d8db
Raddzor01 <danil.garin.04@gmail.com>
2023-04-07 00:50:15 +0300

    Hotfix

--- tests.sh
+++ tests.sh
@@ -5,12 +5,18 @@ if [ ! -f "./uls" ]; then
 fi
 
 declare -A TESTS=(
-    ["test1_desc"]="Print current directory" ["test1_cmd"]="diff <(./uls) <(ls)"
-    ["test2_desc"]="Print root directory" ["test2_cmd"]="diff <(./uls /) <(ls /)"
-    ["test3_desc"]="Print long listing format(-l) - Test 1" ["test3_cmd"]="diff <(./uls -l /) <(ls -l /)"
-    ["test4_desc"]="Print long listing format(-l) - Test 2" ["test4_cmd"]="diff <(./uls -l /dev/null) <(ls -l /dev/null)"
-    ["test5_desc"]="Print long listing format(-l) - Test 3" ["test5_cmd"]="diff <(./uls -l /usr/bin) <(ls -l /usr/bin)"
-    ["test6_desc"]="Print link" ["test6_cmd"]="diff <(./uls ./src) <(ls ./src)"
+    ["test1_desc"]="Print current directory" 
+    ["test1_cmd"]="diff <(./uls) <(ls)"
+    ["test2_desc"]="Print root directory" 
+    ["test2_cmd"]="diff <(./uls /) <(ls /)"
+    ["test3_desc"]="Print long listing format(-l) - Test 1" 
+    ["test3_cmd"]="diff <(./uls -l /) <(ls -l /)"
+    ["test4_desc"]="Print long listing format(-l) - Test 2" 
+    ["test4_cmd"]="diff <(./uls -l /dev/null) <(ls -l /dev/null)"
+    ["test5_desc"]="Print long listing format(-l) - Test 3" 
+    ["test5_cmd"]="diff <(./uls -l /usr/bin) <(ls -l /usr/bin)"
+    ["test6_desc"]="Print link" 
+    ["test6_cmd"]="diff <(./uls ./src) <(ls ./src)"
 )
 
 key=0
