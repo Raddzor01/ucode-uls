f9f1c16155b9bba8ba7b2a7af5782765e8d6e6cc
Raddzor01 <danil.garin.04@gmail.com>
2023-04-07 01:02:16 +0300

    Hotfix

--- tests.sh
+++ tests.sh
@@ -15,27 +15,61 @@ TESTS=(
     ["test4_cmd"]="diff <(./uls -l /dev/null) <(ls -l /dev/null)"
     ["test5_desc"]="Print long listing format(-l) - Test 3" 
     ["test5_cmd"]="diff <(./uls -l /usr/bin) <(ls -l /usr/bin)"
-    ["test6_desc"]="Print link" ["test6_cmd"]="diff <(./uls ./src) <(ls ./src)"
+    ["test6_desc"]="Print link" 
+    ["test6_cmd"]="diff <(./uls ./src) <(ls ./src)"
 )
 
+desc_index=0
+cmd_index=0
 
-for i in $(seq 1 ${#TESTS[@]}) # итерируем от 1 до количества элементов в массиве
+for i in "${!TESTS[@]}"
 do
-    desc_key="test${i}_desc"
-    cmd_key="test${i}_cmd"
+    if [[ $i == *"desc"* ]]; then
+        ((desc_index++))
+        cmd_index=$((desc_index*2))
 
-    # Вывод описания теста
-    echo "${TESTS[$desc_key]}"
-
-    # Выполнение тестового сценария
-    OUTPUT=$(eval "${TESTS[$cmd_key]}")
-
-    # Проверка результата выполнения теста
-    if [ "$OUTPUT" != "" ]
-    then
-        echo "Test failed: ${TESTS[$desc_key]}"
-        echo "$OUTPUT"
+        # Вывод описания теста
+        echo "Starting test $desc_index"
+        echo "${TESTS[$i]}"
     else
-        echo "Success: ${TESTS[$desc_key]}"
+        ((cmd_index++))
+
+        # Выполнение тестового сценария
+        OUTPUT=$(eval "${TESTS[$i]}")
+
+        # Проверка результата выполнения теста
+        if [ "$OUTPUT" != "" ]
+        then
+            echo "Test failed: ${TESTS[$(($cmd_index-1))]}"
+            echo "$OUTPUT"
+        else
+            echo "Success: ${TESTS[$(($cmd_index-1))]}"
+        fi
+    fi
+
+    if [ $cmd_index -eq ${#TESTS[@]} ]
+    then
+        break
     fi
 done
+
+# for i in $(seq 1 ${#TESTS[@]}) # итерируем от 1 до количества элементов в массиве
+# do
+#     desc_key="test${i}_desc"
+#     cmd_key="test${i}_cmd"
+
+#     # Вывод описания теста
+#     echo "Starting test ${TESTS[$desc_key]}..."
+
+#     # Выполнение тестового сценария
+#     OUTPUT=$(eval "${TESTS[$cmd_key]}")
+
+#     # Проверка результата выполнения теста
+#     if [ "$OUTPUT" != "" ]
+#     then
+#         echo "Failed:"
+#         echo "$OUTPUT"
+#     else
+#         echo "Success"
+#     fi
+# done
