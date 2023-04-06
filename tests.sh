bd491881abfac0827ea651258e7d104185c1f187
Raddzor01 <danil.garin.04@gmail.com>
2023-04-06 23:41:09 +0300

    All basic tests

--- tests.sh
+++ tests.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-TEST=$(diff <(./uls) <(ls))
+make reinstall
 
-if [ "$TEST" != ""]
-then
-    echo "Test 1 failed - (print current directory)"
-    echo "$TEST"
-else
-    echo "Success test 1"
-fi
+TESTS=(
+    ["desc"]="Start Basic tests"
+    ["desc"]="Print current directory" ["cmd"]="diff <(./uls) <(ls)"
+    ["desc"]="Print root directory" ["cmd"]="diff <(./uls /) <(ls /)"
+    ["desc"]="Print long listing format(-l) - Test 1" ["cmd"]="diff <(./uls -l /) <(ls -l /)"
+    ["desc"]="Print long listing format(-l) - Test 2" ["cmd"]="diff <(./uls -l /dev/null) <(ls -l /dev/null)"
+    ["desc"]="Print long listing format(-l) - Test 3" ["cmd"]="diff <(./uls -l /usr/bin) <(ls -l /usr/bin)"
+    ["desc"]="Print link" ["cmd"]="diff <(./uls ./src) <(ls -l ./src)"
+    ["desc"]="End Basic tests"
+)
 
-TEST=$(diff <(./uls /) <(ls) /)
+for i in "${!TESTS[@]}"
+do
+    if [[ $i == *"desc"* ]]; then
+        echo "${TESTS[$i]}"
+    else
+        if [ -n "${TESTS[$i]}" ]; then
+            OUTPUT=$(eval "${TESTS[$i]}")
+        else
+            OUTPUT=$(./uls -a)
+        fi
 
-if [ "$TEST" != ""]
-then
-    echo "Test 2 failed - (print root directory)"
-    echo "$TEST"
-else
-    echo "Success test 2"
-fi
+        if [ "$OUTPUT" != "" ]
+        then
+            echo "Test failed: ${TESTS[$i-1]}"
+            echo "$OUTPUT"
+        else
+            echo "Success: ${TESTS[$i-1]}"
+        fi
+    fi
+done
