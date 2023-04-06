ee1c140561526aa943be3a0a3310ea5d84c0c51c
Raddzor01 <danil.garin.04@gmail.com>
2023-04-07 00:39:39 +0300

    Hotfix

--- tests.sh
+++ tests.sh
@@ -5,17 +5,23 @@ if [ ! -f "./uls" ]; then
 fi
 
 declare -A TESTS=(
-    ["desc"]="Print current directory" ["cmd"]="diff <(./uls) <(ls)"
-    ["desc"]="Print root directory" ["cmd"]="diff <(./uls /) <(ls /)"
-    ["desc"]="Print long listing format(-l) - Test 1" ["cmd"]="diff <(./uls -l /) <(ls -l /)"
-    ["desc"]="Print long listing format(-l) - Test 2" ["cmd"]="diff <(./uls -l /dev/null) <(ls -l /dev/null)"
-    ["desc"]="Print long listing format(-l) - Test 3" ["cmd"]="diff <(./uls -l /usr/bin) <(ls -l /usr/bin)"
+    ["desc"]="Print current directory" 
+    ["cmd"]="diff <(./uls) <(ls)"
+    ["desc"]="Print root directory"
+    ["cmd"]="diff <(./uls /) <(ls /)"
+    ["desc"]="Print long listing format(-l) - Test 1"
+    ["cmd"]="diff <(./uls -l /) <(ls -l /)"
+    ["desc"]="Print long listing format(-l) - Test 2" 
+    ["cmd"]="diff <(./uls -l /dev/null) <(ls -l /dev/null)"
+    ["desc"]="Print long listing format(-l) - Test 3" 
+    ["cmd"]="diff <(./uls -l /usr/bin) <(ls -l /usr/bin)"
     ["desc"]="Print link" ["cmd"]="diff <(./uls ./src) <(ls ./src)"
 )
 
+key = 0
 for i in "${!TESTS[@]}"
 do
-    echo "Starting test $i"
+    echo "Starting test $key"
     if [[ $i == *"desc"* ]]; then
         # Вывод описания теста
         echo "${TESTS[$i]}"
@@ -31,5 +37,6 @@ do
         else
             echo "Success: ${TESTS[$i]}"
         fi
+        ((i++))
     fi
 done
